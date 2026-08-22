46c29bd1bacaab70dffb59f2f59b6364bcafded1
kingsleyjudewin <kingsleyjude2025@gmail.com>
2026-08-23 01:26:33 +0530

    Add BACKEND/node-backend/src/modules/boardrooms/boardroom.routes.js (part 3/4)

--- BACKEND/node-backend/src/modules/boardrooms/boardroom.routes.js
+++ BACKEND/node-backend/src/modules/boardrooms/boardroom.routes.js
@@ -8,3 +8,7 @@ const router = Router();
 router.use(authenticate);
 
 router.post('/', validate(createBoardroomSchema), boardroomController.create);
+router.get('/', boardroomController.list);
+router.get('/:id', boardroomController.getById);
+router.get('/:id/messages', boardroomController.getMessages);
+router.post('/:id/members', validate(addMemberSchema), boardroomController.addMember);
