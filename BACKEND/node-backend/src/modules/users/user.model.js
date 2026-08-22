e2b0648d011fe20ffb07da2ef2a81ebe4051ce1d
kingsleyjudewin <kingsleyjude2025@gmail.com>
2026-08-23 01:26:34 +0530

    Add BACKEND/node-backend/src/modules/users/user.model.js (part 4/4)

--- BACKEND/node-backend/src/modules/users/user.model.js
+++ BACKEND/node-backend/src/modules/users/user.model.js
@@ -23,3 +23,11 @@ userSchema.set('toJSON', {
   virtuals: true,
   transform: (_doc, ret) => {
     ret.id = ret._id.toString();
+    delete ret._id;
+    delete ret.__v;
+    delete ret.passwordHash;
+    return ret;
+  },
+});
+
+export const User = mongoose.model('User', userSchema);
