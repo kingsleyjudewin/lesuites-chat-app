a3e3879e37b394430ef9480f388024d9c35167a3
kingsleyjudewin <kingsleyjude2025@gmail.com>
2026-08-23 01:26:33 +0530

    Add BACKEND/node-backend/src/modules/activity/activity.controller.js (part 3/4)

--- BACKEND/node-backend/src/modules/activity/activity.controller.js
+++ BACKEND/node-backend/src/modules/activity/activity.controller.js
@@ -4,3 +4,6 @@ import { catchAsync } from '../../utils/catchAsync.js';
 export const getUserActivity = catchAsync(async (req, res) => {
   const feed = await activityService.getFeed(req.params.id);
   res.json({ success: true, data: feed });
+});
+
+export const getSidebar = catchAsync(async (req, res) => {
