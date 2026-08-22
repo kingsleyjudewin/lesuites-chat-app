fc0cc4acc6ddc8f940d3271abe6ab99dbfde377f
kingsleyjudewin <kingsleyjude2025@gmail.com>
2026-08-23 01:26:35 +0530

    Add frontend/src/App.jsx (part 1/4)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { SocketProvider } from './context/SocketContext';
+import { ProtectedRoute } from './components/ProtectedRoute';
+import { LoginPage } from './pages/LoginPage';
+import { HomePage } from './pages/HomePage';
+import { MessagingPage } from './pages/MessagingPage';
+import { BoardroomPage } from './pages/BoardroomPage';
