066413f8267266df25f4a21c63d7c713413fcd36
kingsleyjudewin <kingsleyjude2025@gmail.com>
2026-08-23 01:26:34 +0530

    Add BACKEND/node-backend/src/modules/presence/presence.controller.js (part 2/4)

--- BACKEND/node-backend/src/modules/presence/presence.controller.js
+++ BACKEND/node-backend/src/modules/presence/presence.controller.js
@@ -2,3 +2,7 @@ import * as presenceService from './presence.service.js';
 import { User } from '../users/user.model.js';
 import { catchAsync } from '../../utils/catchAsync.js';
 import { ApiError } from '../../utils/ApiError.js';
+
+export const getStatus = catchAsync(async (req, res) => {
+  const user = await User.findById(req.params.userId).select('status lastSeen');
+  if (!user) throw new ApiError(404, 'User not found');
