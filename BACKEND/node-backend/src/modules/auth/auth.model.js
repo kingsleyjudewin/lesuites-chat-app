89ccd51ded252d4cf62e9dcdfecab8a656ae86f5
kingsleyjudewin <kingsleyjude2025@gmail.com>
2026-08-23 01:26:33 +0530

    Add BACKEND/node-backend/src/modules/auth/auth.model.js (part 1/4)

---
+++ BACKEND/node-backend/src/modules/auth/auth.model.js
@@ -0,0 +1,5 @@
+import mongoose from 'mongoose';
+
+const refreshTokenSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true },
