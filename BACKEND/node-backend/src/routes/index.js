457fafe25694376e4cd18b4d424c519f846dcfc4
kingsleyjudewin <kingsleyjude2025@gmail.com>
2026-08-23 01:26:34 +0530

    Add BACKEND/node-backend/src/routes/index.js (part 1/4)

---
+++ BACKEND/node-backend/src/routes/index.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import authRoutes from '../modules/auth/auth.routes.js';
+import userRoutes from '../modules/users/user.routes.js';
+import conversationRoutes from '../modules/conversations/conversation.routes.js';
+import messageRoutes from '../modules/messages/message.routes.js';
+import boardroomRoutes from '../modules/boardrooms/boardroom.routes.js';
+import presenceRoutes from '../modules/presence/presence.routes.js';
