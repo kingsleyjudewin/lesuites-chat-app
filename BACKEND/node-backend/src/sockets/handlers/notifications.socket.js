87b8bf0e7a7dbb35a8a0ee3cef88ab94f7766c38
kingsleyjudewin <kingsleyjude2025@gmail.com>
2026-08-23 01:26:34 +0530

    Add BACKEND/node-backend/src/sockets/handlers/notifications.socket.js (part 1/4)

---
+++ BACKEND/node-backend/src/sockets/handlers/notifications.socket.js
@@ -0,0 +1,4 @@
+import * as notificationService from '../../modules/notifications/notification.service.js';
+import { logger } from '../../utils/logger.js';
+
+export function registerNotificationHandlers(io, socket) {
