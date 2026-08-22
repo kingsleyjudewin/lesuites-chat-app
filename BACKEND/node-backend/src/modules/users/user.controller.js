27bdb08ffaf1f6b4bcd05fdfd52cfbe030f4049a
kingsleyjudewin <kingsleyjude2025@gmail.com>
2026-08-23 01:26:34 +0530

    Add BACKEND/node-backend/src/modules/users/user.controller.js (part 4/4)

--- BACKEND/node-backend/src/modules/users/user.controller.js
+++ BACKEND/node-backend/src/modules/users/user.controller.js
@@ -15,3 +15,8 @@ export const getById = catchAsync(async (req, res) => {
   const user = await userService.getById(req.params.id);
   res.json({ success: true, data: user });
 });
+
+export const search = catchAsync(async (req, res) => {
+  const result = await userService.search(req.query);
+  res.json({ success: true, data: result });
+});
