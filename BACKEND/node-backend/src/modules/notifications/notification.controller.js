f69adee56071292ecceb14f8d024df4506337e65
kingsleyjudewin <kingsleyjude2025@gmail.com>
2026-08-23 01:26:34 +0530

    Add BACKEND/node-backend/src/modules/notifications/notification.controller.js (part 2/4)

--- BACKEND/node-backend/src/modules/notifications/notification.controller.js
+++ BACKEND/node-backend/src/modules/notifications/notification.controller.js
@@ -1,3 +1,6 @@
 import * as notificationService from './notification.service.js';
 import { catchAsync } from '../../utils/catchAsync.js';
 
+export const list = catchAsync(async (req, res) => {
+  const notifications = await notificationService.listForUser(req.user.id);
+  res.json({ success: true, data: notifications });
