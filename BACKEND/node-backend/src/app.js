be71bdac6bbd1d5f98fe628bd6b2aba8a54da84f
kingsleyjudewin <kingsleyjude2025@gmail.com>
2026-08-23 01:26:32 +0530

    Add BACKEND/node-backend/src/app.js (part 1/4)

---
+++ BACKEND/node-backend/src/app.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import mongoSanitize from 'express-mongo-sanitize';
+import { env } from './config/env.js';
