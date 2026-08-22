9ff20c7359d670f54d2c2940dcaf809a7cecdf42
kingsleyjudewin <kingsleyjude2025@gmail.com>
2026-08-23 01:26:33 +0530

    Add BACKEND/node-backend/src/modules/activity/activityEvent.model.js (part 2/4)

--- BACKEND/node-backend/src/modules/activity/activityEvent.model.js
+++ BACKEND/node-backend/src/modules/activity/activityEvent.model.js
@@ -4,3 +4,9 @@ import { ACTIVITY_TYPE } from '../../config/constants.js';
 const activityEventSchema = new mongoose.Schema(
   {
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true },
+    type: { type: String, enum: Object.values(ACTIVITY_TYPE), required: true },
+    refId: { type: mongoose.Schema.Types.ObjectId, default: null },
+  },
+  { timestamps: true }
+);
+
