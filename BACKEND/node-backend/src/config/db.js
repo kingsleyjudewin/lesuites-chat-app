32bd45c15fc6d75fd09d9db8a801d08ee8488361
kingsleyjudewin <kingsleyjude2025@gmail.com>
2026-08-23 01:26:33 +0530

    Add BACKEND/node-backend/src/config/db.js (part 2/4)

--- BACKEND/node-backend/src/config/db.js
+++ BACKEND/node-backend/src/config/db.js
@@ -2,3 +2,6 @@ import mongoose from 'mongoose';
 import { env } from './env.js';
 import { logger } from '../utils/logger.js';
 
+export async function connectDB() {
+  mongoose.set('strictQuery', true);
+  await mongoose.connect(env.MONGO_URI);
