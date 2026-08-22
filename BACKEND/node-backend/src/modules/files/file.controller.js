f4d932d96ea2bde85f3d332485f2f1de83de29b4
kingsleyjudewin <kingsleyjude2025@gmail.com>
2026-08-23 01:26:33 +0530

    Add BACKEND/node-backend/src/modules/files/file.controller.js (part 2/4)

--- BACKEND/node-backend/src/modules/files/file.controller.js
+++ BACKEND/node-backend/src/modules/files/file.controller.js
@@ -4,3 +4,9 @@ import { catchAsync } from '../../utils/catchAsync.js';
 export const presign = catchAsync(async (req, res) => {
   const result = await fileService.createPresignedUpload(req.user.id, req.body);
   res.json({ success: true, data: result });
+});
+
+export const confirm = catchAsync(async (req, res) => {
+  const file = await fileService.confirmUpload(req.user.id, req.body);
+  res.status(201).json({ success: true, data: file });
+});
