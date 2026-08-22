6149f08f7f6acf2bc8f7f7e0a9ef3f9052270511
kingsleyjudewin <kingsleyjude2025@gmail.com>
2026-08-23 01:26:35 +0530

    Add frontend/src/lib/socket.js (part 2/4)

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -4,3 +4,9 @@ const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 let socket = null;
 
+export function connectSocket(token) {
+  if (socket) socket.disconnect();
+  socket = io(SOCKET_URL, { auth: { token }, transports: ['websocket'] });
+  return socket;
+}
+
